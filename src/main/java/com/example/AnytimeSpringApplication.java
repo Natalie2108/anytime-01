e9e3f49fa158a40bf6f5289512ede62af98cb82f
REP <natalie.kersten@hotmail.com>
2020-05-11 11:21:35 +0200

    Spring-angular spring boot app aangepast

--- src/main/java/com/example/AnytimeSpringApplication.java
+++ src/main/java/com/example/AnytimeSpringApplication.java
@@ -2,9 +2,15 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AnytimeSpringApplication {
+public class AnytimeSpringApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(AnytimeSpringApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnytimeSpringApplication.class, args);
